f4a5b04be9e9909f987c25be23dff24f3737671f
Alexey Matveev <asm@metaphysics-it.net>
2013-07-28 20:22:22 +0400

    Response data classes implemented

---
+++ BloggerClient/Blogger.Data/Responses/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blogger.Data.Responses
+{
+    public class ErrorResponse
+    {
+        public ServerErrorResponse RawResult { get; set; }
+
+        public Exception Error { get; set; }
+    }
+}
